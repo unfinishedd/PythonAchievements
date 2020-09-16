cc7128560cb915a74a919700738bf223e6e77f80
Sam <sam0000@live.nl>
2020-09-16 12:32:44 +0200

    Player Statistics

---
+++ PYTB1L2PlayerStatistics/PlayerStatistics.py
@@ -0,0 +1,52 @@
+
+name = "LaMelo Ball"
+print("Name: " + name)
+
+length= "6ft8"
+print("Length: " + length)
+
+weight= 180
+print("Weight: " + weight)
+
+position= "Point Guard"
+print("Position: " + position)
+
+jumpshot= 8
+print("Jumpshot: " + jumpshot)
+
+strength= 7
+print("Strength: " + strength)
+
+defense= 7
+print("Defense: " + defense)
+
+passing= 9
+print("Passing: " + passing)
+
+intangibles= 8
+print("Intangibles: " + intangibles)
+
+size= 9
+print("Size: " + size)
+
+nba ready= 7
+print("NBA Ready: " + nba ready)
+
+ball handling= 9
+print("Ball Handling: " + ball handling)
+
+quickness= 8
+print("Quickness: " + quickness)
+
+athleticism= 7
+print("Athleticism: " + athleticism)
+
+leadership= 8
+print("Leadership: " + leadership)
+
+potential= 9
+print("Potential: " + potential)
+
+
+overall= 96
+print("Overall: " + overall)
