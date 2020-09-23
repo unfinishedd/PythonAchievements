27397da22af0d8e19f748b64f373d64220526bd4
Sam <sam0000@live.nl>
2020-09-23 11:57:22 +0200

    poem printing

---
+++ PYTB1L2PoemPrinting/PoemPrinting.py
@@ -0,0 +1,15 @@
+vers1 = " Nooit begrepen waarom sommige mensen bepaalde spelers kunnen haten zonder enige reden. "
+vers2 = " Werken is niet mijn ding maar ik doe het toch en verwennen was me niet geleerd. "
+vers3 = " Springen doe ik hoog, het is ook wel simpel sinds het zich in mijn gene bevind. "
+vers4 = " Basketbal is simpel voor mij en dat kan je me niet betwijfelen. "
+vers5 = " Verticale records gebroken op mijn oude school met mijn lengte is niet zo kenmerkent. "
+vers6 = " Moedernatuur bezoek ik maar ik kom niet tussen zijn harmonie. "
+vers7 = " Energie verdwijnt nooit dus laat ik het mijn advies achter in een andere dimentie. "
+
+print(vers1)
+print(vers2)
+print(vers3)
+print(vers4)
+print(vers5)
+print(vers6)
+print(vers7)
