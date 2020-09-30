df372830b6b406020f4c75c59cb2aff966f93cbc
Sam <sam0000@live.nl>
2020-09-30 13:02:01 +0200

    idk

---
+++ PYTB1L2HowYouLikeThemApples/HowYouLikeThemApples.py
@@ -0,0 +1,42 @@
+#333 appelbomen
+#2/3 deel van bomen in schaduw, geeft 80%
+#in de zon= 146 appels
+
+#appels delen door 4 vanwege 3 andere vrienden.
+#hoeveel kan ik er verkopen
+
+
+
+getalA = 333
+
+getalA /= 3
+
+print(getalA)
+
+
+getalB = 
+
+getalB = 
+
+print(getalB)
+
+
+getalC = 
+
+getalC = 
+
+print(getalC)
+
+
+getalD = 
+
+getalD = 
+
+print(getalD)
+
+
+getalE = 
+
+getalE = 
+
+print(getalE)
