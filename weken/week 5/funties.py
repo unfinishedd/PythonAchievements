e2fc4d217a91f829687b4f0493d70858fbfd912d
Sam <sam0000@live.nl>
2020-10-02 12:27:25 +0200

    nieuwe mappen

---
+++ weken/week 5/funties.py
@@ -0,0 +1,9 @@
+def FunctieMetParameters (waardeA, waardeB, waardeC):
+
+
+    print("Waarde A is: ", waardeA)
+    print("Waarde B is: ", waardeB)
+    print("Waarde C is: ", waardeC)
+    print("______________________________")
+
+
