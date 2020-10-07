ee92352c8fd4bb307848edfc7f72bb10f443848b
Sam <sam0000@live.nl>
2020-10-07 12:07:41 +0200

    InterestingTellMeMore

---
+++ PYTB1L2InterestingTellMeMore/InterestingTellMeMore.py
@@ -0,0 +1,18 @@
+name = input("Wat is je naam? ")
+
+print("Oke oke, dus jij heet " + name + ", nice!")
+
+
+age = input("Hoe oud ben je? ")
+
+print("Aha, je bent dus " + age + " jaar oud, lekker man!")
+
+
+woonplaats = input("Dus, waar woon je eigenlijk? ")
+
+print("Hmmm, " + woonplaats + " is waarschijnlijk wel een leuke stad.")
+
+
+interesse = input("Sinds je in " + woonplaats + " woont, wat kan je daar allemaal doen? ")
+
+print("Dus je kan " + interesse + ", dat is wel nice.")
