f6b359289779e9d2af47492f02439272e5f47748
unfinishedd <sam0000@live.nl>
2020-10-21 12:37:44 +0200

    WheresWaldo

---
+++ PYTB1L4WheresWaldo/WheresWaldo.py
@@ -0,0 +1,9 @@
+import random
+people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert","Robert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha","Tory","Troy", "Faye"]
+random.shuffle(people)
+
+index = people.index('Waldo')
+
+print("Waldo zit op nummer",index,)
+
+print(people)
