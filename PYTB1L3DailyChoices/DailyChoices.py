aa33b6ebdcc6207347db3f048dc3e46406a74c63
unfinishedd <sam0000@live.nl>
2020-10-21 12:12:51 +0200

    Daily Choices

---
+++ PYTB1L3DailyChoices/DailyChoices.py
@@ -0,0 +1,44 @@
+print("Your alarm rings, do you GETUP or SNOOZE ?")
+choice = input()
+if choice == 'GETUP':
+    print("You get out of bed and just do your thing!")
+elif choice == 'SNOOZE':
+    print("Hmmmmmm zzzzzzzzzzzzzzzzzzzzzzzzzz")
+else:
+    print(choice, " wasn't a good enough choice, try again ")
+
+print("Your employer is calling you after you broke the icecream machine, do you wish to PICK UP the phone of LEAVE IT ? ")
+choice = input()
+if choice == 'PICK UP':
+    print("Your employer starts shouting at you and tells you to pack your bags and leave")
+elif choice == 'LEAVE IT':
+    print("You go to your work where you get fired right on the spot, F ")
+else:
+    print(choice, " you didn't pick any of the choices, it's crazy how you wasn't fired years ago")
+
+print("Your bus is leaving in 5 minutes do you wish to RUN for the bus or WAIT for the next one?")
+choice = input()
+if choice == 'RUN':
+    print("You're too slow anyway so just wait for the next one! ")
+elif choice == 'WAIT':
+    print("Well... you look at the arrival time and it says 20 minutes, well done ")
+else:
+    print(choice, " wasn't the right choice, try again ")
+
+print("Your parents are having the whole family over do you wish to ACT DECENT or PAY NO ATTENTION to anyone? ")
+choice = input()
+if choice == 'ACT DECENT':
+    print(" You proceed to act decent and everyone starts to like you to the point where they won't leave you alone... well done... ")
+elif choice == 'PAY NO ATTENTION':
+    print(" You proceed to go on you phone and ignore everyone, start preparing yourself for your asswooping ")
+else:
+    print(choice, " -Some people are just too dumb to choose a regular choice or just don't know how to spell ")
+
+print("Your email tells you that you've won a free €100 bol.com giftcard, do you wish to OPEN the link or DELETE the email? ")
+choice = input()
+if choice == 'OPEN':
+    print(" The link sends you to a site where you actually proceed to win the €100 giftcard!")
+elif choice == 'DELETE':
+    print(" You tried to click the delete button but you failed and it proceeds to download a trojan on your pc, well done...")
+else:
+    print(choice, " wasn't the right choice make a better decision ")
