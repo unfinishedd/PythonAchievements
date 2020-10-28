bf648aa78d56e08a5aa244d88c27509f758ae3df
unfinishedd <sam0000@live.nl>
2020-10-28 11:33:29 +0100

    opdrachten af

---
+++ PYTB1L6TransportItems/TransportItems.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+factory = ["Playstation 5", "PS3", "PS4"]
+distribution = []
+shop = []
+
+def factory_to_distribution():
+    distribution.append(factory.pop())
+    
+
+
+def distribution_to_shop():
+    shop.extend(distribution)
+    distribution.pop(0)
+
+def printlist():
+        print("factory = " + str(factory))
+        print("distribution = "+ str(distribution))
+        print("shop = "+ str(shop))
+    
+def nextprint():
+    time.sleep(2.1)
+    os.system('clear')
+
+printlist()
+factory_to_distribution()
+nextprint()
+printlist()
+distribution_to_shop()
+nextprint()
+printlist()
